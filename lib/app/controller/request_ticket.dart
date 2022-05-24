8fff7dbc69132911cb802011710e8f4d5c4c8fdb
Long9725 <develop9725@gmail.com>
2022-05-24 23:23:58 +0900

    Add: request_ticket page ui

--- lib/app/controller/request_ticket.dart
+++ lib/app/controller/request_ticket.dart
@@ -1,21 +1,11 @@
-import 'package:enft/app/controller/gym_explore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' as foundation;
+
 import 'package:get/get.dart';
 
+import 'package:enft/app/controller/gym_explore.dart';
 import 'package:enft/app/data/repository/request_ticket.dart';
-
-// 시나리오
-// 1. 게시글에서 A 헬스장 사장님이 헬스장 이용권 홍보 및 판매 게시글을 올린다.
-// 2. 사용자는 그 글을 보고 마음에 들어 발급 요청 버튼을 누른다.
-// 3. 사용자는 헬스장 이용권 기간과 지불할 클레이튼을 입력한다.
-// 4. 사용자는 완료 버튼을 눌러 발급 요청을 하고, 클레이튼을 지불한다.
-// 5. A 헬스장 사장님은 발급 요청을 확인하고, 발급을 승인한다.
-// 6. 사용자는 이용권을 받는다.
-// 7. 만약 승인하지 않는다면, 클레이튼을 환불받는다.
-
-// 개발
-// 1. 사용자가 발급 요청 버튼을 누를 때, 헬스장 이름은 고정이며 헬스장 이용권 기간 목록 데이터를 받아와야 한다.
-// 2. 헬스장 이름은 텍스트, 헬스장 이용권 기간 목록은 드롭다운, 클레이튼은 텍스트폼이다.
-// 3. 완료 버튼을 누르면 발급 요청과 더불어 클레이튼을 지불한다.
 
 class RequestTicketController extends GetxController {
   late final RequestTicketRepository repository;
@@ -62,4 +52,11 @@ class RequestTicketController extends GetxController {
 //   _gymNames = repository.getGymNames();
 //   selectGymName = gymNames[0];
 // }
+  void openDialog(String title, String content, List<Widget> actions) {
+    Get.dialog(foundation.defaultTargetPlatform == foundation.TargetPlatform.iOS
+        ? CupertinoAlertDialog(
+        title: Text(title), content: Text(content), actions: actions)
+        : AlertDialog(
+        title: Text(title), content: Text(content), actions: actions));
+  }
 }
--- lib/app/ui/request_ticket/request_ticket.dart
+++ lib/app/ui/request_ticket/request_ticket.dart
@@ -1,13 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'package:enft/app/constant/constant.dart';
+import 'package:enft/app/controller/user.dart';
+import 'package:enft/app/controller/request_ticket.dart';
 
 import 'package:enft/app/ui/request_ticket/components/body.dart';
 
 import '../../controller/gym_explore.dart';
 
-class RequestTicketPage extends StatelessWidget {
+class RequestTicketPage extends GetView {
+  String place = Get.arguments["tag"];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(), body: RequestTicketBody());
+    final controller =
+        Get.find<RequestTicketController>(tag: Get.arguments['tag']);
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: RequestTicketBody(),
+      bottomNavigationBar: buildBottomNavigationBar(controller),
+    );
   }
 
   AppBar buildAppBar() {
@@ -19,5 +35,102 @@ class RequestTicketPage extends StatelessWidget {
       title: Text("이용권 발급 요청"),
       actions: [],
     );
+  }
+
+  BottomAppBar buildBottomNavigationBar(RequestTicketController controller) {
+    return BottomAppBar(
+        color: Colors.white,
+        elevation: 0,
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          const Divider(
+            height: 1,
+            thickness: 1,
+          ),
+          Padding(
+            padding: EdgeInsets.all(kDefaultPadding),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                    child: Obx(() => Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "발급 요청 정보",
+                              style: TextStyle(
+                                color: kSystemGray,
+                                fontSize: 12,
+                              ),
+                            ),
+                            Text(
+                              controller.selectKlayInfo.month.toString() + " 달",
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: kDefaultPadding / 8),
+                                child: Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        (controller.selectKlayInfo.klay)
+                                            .toStringAsFixed(2),
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: kDefaultPadding / 4,
+                                      ),
+                                      Text("KLAY")
+                                    ])),
+                          ],
+                        ))),
+                // Expanded(
+                //     child: Text(currencyFormat(postList[0].price),
+                //         style: const TextStyle(
+                //             fontWeight: FontWeight.bold, fontSize: 18))),
+                OutlinedButton(
+                  onPressed: () async {
+                    bool isTransferable = (controller.selectKlayInfo.klay >
+                            UserController.to.user.klip.balance)
+                        ? false
+                        : true;
+                    if (isTransferable) {
+                      KlayData klayInfo = controller.selectKlayInfo;
+                      final response = await UserController.to
+                          .RequestAuth("POST", "/user/approve", data: {
+                        "requestPlace": place,
+                        "requestDay": klayInfo.month * 30,
+                      });
+                      print(response.statusCode);
+                      Map<String, dynamic> body = jsonDecode(response.body);
+                      if (response.statusCode == 201) {
+                        Get.back();
+                      } else {
+                        Get.snackbar('Fail', body["message"],
+                            snackPosition: SnackPosition.TOP);
+                      }
+                    } else {
+                      controller.openDialog("잔고 부족", "클레이가 충분하지 않습니다.", [
+                        TextButton(
+                            onPressed: () => Get.back(), child: Text("확인"))
+                      ]);
+                    }
+                  },
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(
+                        horizontal: kDefaultPadding * 2,
+                        vertical: kDefaultPadding / 2)),
+                    backgroundColor: MaterialStateProperty.all(kPrimaryColor),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                  ),
+                  child: const Text(
+                    "전송하기",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                )
+              ],
+            ),
+          )
+        ]));
   }
 }
