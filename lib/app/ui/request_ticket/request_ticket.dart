1769013a6a8e9f676ef53ba4435819ed927e30e7
Long9725 <develop9725@gmail.com>
2022-05-24 22:40:32 +0900

    Add: before git pull

--- lib/app/ui/request_ticket/request_ticket.dart
+++ lib/app/ui/request_ticket/request_ticket.dart
@@ -7,11 +7,16 @@ import '../../controller/gym_explore.dart';
 class RequestTicketPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(appBar: buildAppBar(), body: RequestTicketBody()));
+    return Scaffold(appBar: buildAppBar(), body: RequestTicketBody());
   }
+
   AppBar buildAppBar() {
     return AppBar(
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
+      elevation: 0,
+      centerTitle: true,
+      title: Text("이용권 발급 요청"),
       actions: [],
     );
   }
