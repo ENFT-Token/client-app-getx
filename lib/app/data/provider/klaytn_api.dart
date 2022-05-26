b93c8965986fbd237d9b71ff67436a4944be009b
gron1gh1 <gron1gh1@gmail.com>
2022-05-26 17:58:24 +0900

    feat: getHistory (nft, klay)

--- lib/app/data/provider/klaytn_api.dart
+++ lib/app/data/provider/klaytn_api.dart
@@ -7,17 +7,19 @@ import 'package:convert/convert.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:http_auth/http_auth.dart' as http_auth;
+import 'package:intl/intl.dart';
 
 
 // KAS: Klaytn Api Service
 class KlaytnApiClient {
-  final basicUri = 'https://node-api.klaytnapi.com/v1';
+  final basicUri = 'https://node-api.klaytnapi.com';
   final authHttp = http_auth.BasicAuthClient(
       dotenv.env['KAS_ACCESS_KEY_ID'] ?? "",
       dotenv.env['KAS_SECRET_ACCESS_KEY'] ?? "");
   final Map<String, String> headers = <String, String>{
     'Content-type': 'application/json'
   };
+  String contractAddress = "";
   final Map<String, String> xChainIdHeaders = <String, String>{
     // real net
     'x-chain-id': '8217',
@@ -26,10 +28,122 @@ class KlaytnApiClient {
     //'x-chain-id': '1001',
     'Content-type': 'application/json'
   };
+
+  final Map<String, String> xChainIdHeadersBaoBab = <String, String>{
+    // test net
+    'x-chain-id': '1001',
+    'Content-type': 'application/json'
+  };
+
+  KlaytnApiClient() {
+    init();
+  }
+
+  void init() async {
+    this.contractAddress = await getContractAddress();
+
+    // 테스트 코드
+    final nftHistory = await getHistory("baobab", "0x5530580E722f5dDEeeFb34b45fA8c5cb382dD789","nft");
+    print("nftHistory ${nftHistory}");
+    final klayHistory = await getHistory("mainnet", "0x5530580E722f5dDEeeFb34b45fA8c5cb382dD789","klay");
+    print("klayHistory ${klayHistory}");
+  }
+
+  /**
+   * 스마트 컨트랙트 주소 가져오기
+   */
+  Future<String> getContractAddress() async {
+    final uri = Uri.parse(dotenv.env['SERVER_ADDRESS']! + "/caver/contract");
+    final http.Response response = await http.get(uri);
+    if(response.statusCode == 200) {
+      return response.body;
+    }
+    return "";
+  }
+
+
+  /**
+   * 서버에서 넘어오는 클레이튼 실제 클레이튼 개수로 변환
+   */
+  double calcHexKlayToDouble(String klayAmount) {
+    double PEB = 1000000000000000000; // KLAY PEB
+    double balance = int.parse(klayAmount.substring(2), radix:16) / PEB;
+    return balance;
+  }
+
+
+  String DateTimeToFormat(DateTime date) {
+    final DateFormat formatter = DateFormat('yyyy-MM-dd HH:mm:ss');
+    final String formatted = formatter.format(date);
+    return formatted;
+  }
+
+  /**
+   * 거래 내역 가져오기
+   * @Argument network "mainnet" | "baobab"
+   * @Argument kind "nft" | "klay"
+   */
+  Future<List<Map<String,dynamic>>> getHistory(String network, String address, String kind) async  {
+    Map<String, dynamic> query = {};
+    if(kind == "nft") {
+      print(kind + " " + contractAddress);
+      query = {
+        'kind': kind,
+        'ca-filter': contractAddress
+      };
+    } else if(kind == "klay"){
+        print(kind + " " + contractAddress);
+        query = {
+          'kind': kind,
+        };
+    }
+    Uri uri = Uri.parse("https://th-api.klaytnapi.com/v2/transfer/account/${address}")
+        .replace(queryParameters: query);
+
+    late final http.Response response;
+    if(network == "mainnet")
+      response = await authHttp.get(uri, headers: xChainIdHeaders);
+    else if(network == "baobab")
+      response = await authHttp.get(uri, headers: xChainIdHeadersBaoBab);
+    if(response.statusCode == 200) {
+      final responseBody = List<Map<String, dynamic>>.from(json.decode(response.body)['items']);
+      if(kind == "nft") {
+        List<Map<String,dynamic>> nftDatas = responseBody.map((item) {
+          DateTime time = DateTime.fromMillisecondsSinceEpoch(item['transaction']['timestamp'] * 1000);
+
+          return {
+            "transferType": item['transferType'],
+            "from": item['from'],
+            "to": item['to'],
+            "time": DateTimeToFormat(time),
+          };
+        }).toList();
+        return nftDatas;
+      }
+
+      else if(kind == "klay") {
+        List<Map<String,dynamic>> klayDatas = responseBody.map((item) {
+          DateTime time = DateTime.fromMillisecondsSinceEpoch(item['timestamp'] * 1000);
+          return {
+            "transferType": item['transferType'],
+            "from": item['from'],
+            "to": item['to'],
+            "value": calcHexKlayToDouble(item['value']),
+            "fee": calcHexKlayToDouble(item['fee']),
+            "status":  item['to'].toUpperCase() == address.toUpperCase() ? "deposit" : "withdraw",
+            "time": DateTimeToFormat(time),
+          };
+        }).toList();
+        return klayDatas;
+      }
+    }
+    return [];
+  }
+
   // Get latest block number
   // response: {"jsonrpc": "2.0", "id":1, "result": "latest block number"}
   Future<String> getBlockNumber() async {
-    Uri uri = Uri.parse(basicUri + '/klaytn');
+    Uri uri = Uri.parse(basicUri + '/v1/klaytn');
     String body = jsonEncode(<String, dynamic>{
       'jsonrpc': '2.0',
       'method': 'klay_blockNumber',
@@ -45,7 +159,7 @@ class KlaytnApiClient {
   // input: userAddress, latest block number
   // response: {"jsonrpc": "2.0", "id":1, "result": Balance string, peb}
   Future<double> getBalance(String userAddress) async {
-    Uri uri = Uri.parse(basicUri + '/klaytn');
+    Uri uri = Uri.parse(basicUri + '/v1/klaytn');
     String blockNumber = await getBlockNumber();
     String body = jsonEncode(<String, dynamic>{
       'jsonrpc': '2.0',
@@ -69,7 +183,7 @@ class KlaytnApiClient {
   // input: NFT contract address and NFT ID
   // response: {"uri": NFT json, "owner": owner address}
   void LookUpNFT(String address, String id) async {
-    Uri uri = Uri.parse(basicUri + '/metadata/nft/$address/$id');
+    Uri uri = Uri.parse(basicUri + '/v1/metadata/nft/$address/$id');
     final http.Response response = await http.get(uri, headers: headers);
   }
 
